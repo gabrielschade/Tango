fb58010efe4ecd9d540d9806155c64f0caf23eed
Gabriel Schade <gabriel.schade@hotmail.com>
2017-01-17 00:23:46 -0200

    Commit inicial da Api

---
+++ Tango/Tango.Core/Extensions/ContinuationExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Tango.Core.Extensions
+{
+    public static class ContinuationExtensions
+    {
+        public static Continuation<TSuccess, TFail> MapToContinuation<TSuccess, TFail>(this TSuccess value)
+            where TFail : FailResult
+            => new Continuation<TSuccess, TFail>(value);
+    }
+}
